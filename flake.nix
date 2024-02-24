4e53f1f59bdc708a73378843765750f72153f8d0
Andrew Jeffery <dev@jeffas.net>
2024-02-24 10:18:26 +0000

    Init flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        rustc
+        cargo
+        rustfmt
+      ];
+    };
+  };
+}
