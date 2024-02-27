8089bb5237df59de72844459762c808ebb4e2a75
Andrew Jeffery <dev@jeffas.net>
2024-02-27 08:27:11 +0000

    Add cargo nix building

--- flake.nix
+++ flake.nix
@@ -9,18 +9,31 @@
   }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
+    cargoNix = pkgs.callPackage ./Cargo.nix {};
   in {
+    checks.${system} = {
+      bwfs = cargoNix.rootCrate.build.override {
+        runTests = true;
+      };
+    };
+
+    packages.${system} = {
+      bwfs = cargoNix.rootCrate.build;
+    };
+
     devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        rustc
-        cargo
-        rustfmt
+      packages = [
+        pkgs.rustc
+        pkgs.cargo
+        pkgs.rustfmt
 
-        pkg-config
-        fuse3
-        openssl
+        pkgs.crate2nix
 
-        bitwarden-cli
+        pkgs.pkg-config
+        pkgs.fuse3
+        pkgs.openssl
+
+        pkgs.bitwarden-cli
       ];
     };
   };
